5bb5520cde78e470641ae2073ff66fec7e3d5b0c
pchien925 <pcchien250904@gmail.com>
2024-09-17 18:44:55 +0700

    hoan thanh chuc nang register, login

--- java/ltweb_weekly_proj/pc/controllers/web/HomeController.java
+++ java/ltweb_weekly_proj/pc/controllers/web/HomeController.java
@@ -2,6 +2,7 @@ package ltweb_weekly_proj.pc.controllers.web;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import ltweb_weekly_proj.pc.models.Email;
 import ltweb_weekly_proj.pc.models.User;
 import ltweb_weekly_proj.pc.services.IUserService;
 import ltweb_weekly_proj.pc.services.Impl.UserServiceImpl;
+import ltweb_weekly_proj.pc.utils.Constant;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -66,10 +68,22 @@ public class HomeController extends HttpServlet {
 
 	private void getLogin(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);
 		if (session != null && session.getAttribute("userAccount") != null) {
-			response.sendRedirect(request.getContextPath() + "/waiting");
+			response.sendRedirect(request.getContextPath() + "/home");
 			return;
+		}
+
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("username")) {
+					session = request.getSession(true);
+					session.setAttribute("username", cookie.getValue());
+					response.sendRedirect(request.getContextPath() + "/home");
+					return;
+				}
+			}
 		}
 		request.getRequestDispatcher("views/web/login.jsp").forward(request, response);
 	}
@@ -191,6 +205,11 @@ public class HomeController extends HttpServlet {
 
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+		boolean isRememberMe = false;
+		String remember = request.getParameter("remember");
+		if ("on".equals(remember)) {
+			isRememberMe = true;
+		}
 
 		String alertMes = "";
 		if (username.isEmpty() || password.isEmpty()) {
@@ -204,11 +223,41 @@ public class HomeController extends HttpServlet {
 		User user = userService.login(username, password);
 
 		if (user != null) {
-			request.getRequestDispatcher("/views/web/home.jsp").forward(request, response);
+			if (user.getStatus() == 1) {
+				HttpSession session = request.getSession(true);
+				session.setAttribute("userAccount", user);
+				if (isRememberMe) {
+					SaveRememberMe(response, username);
+				}
+
+				request.getRequestDispatcher("/views/web/home.jsp").forward(request, response);
+			} else {
+				alertMes = "Tài khoản đã bị khóa";
+				request.setAttribute("error", alertMes);
+				request.getRequestDispatcher("/views/web/login.jsp").forward(request, response);
+			}
+
 		} else {
 			alertMes = "Sai tài khoản hoặc mật khẩu";
 			request.setAttribute("message", alertMes);
 			request.getRequestDispatcher("/views/web/login.jsp").forward(request, response);
 		}
 	}
+
+	private void SaveRememberMe(HttpServletResponse response, String username) {
+		Cookie cookie = new Cookie(Constant.COOKIE_REMEMBER, username);
+		cookie.setMaxAge(1800);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(true);
+		cookie.setPath("/");
+		response.addCookie(cookie);
+	}
+
+	private void removeRememberMe(HttpServletResponse response) {
+		Cookie cookie = new Cookie(Constant.COOKIE_REMEMBER, null);
+		cookie.setMaxAge(0);
+		cookie.setPath("/"); 
+		response.addCookie(cookie);
+	}
+
 }
---
+++ java/ltweb_weekly_proj/pc/utils/Constant.java
@@ -0,0 +1,6 @@
+package ltweb_weekly_proj.pc.utils;
+
+public class Constant {
+	public static final String SESSION_USERNAME = "username";
+	public static final String COOKIE_REMEMBER = "username";
+}
